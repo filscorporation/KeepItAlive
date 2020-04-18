1fb7446831b8121b1c31b6ca861a5f6018de8230
Filipp <louieandi@gmail.com>
2020-04-18 16:44:12 +0300

    Enemy class. First enemy - bee

---
+++ Assets/Source/Bee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Animations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Source
+{
+    /// <summary>
+    /// Bee
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public class Bee : Enemy
+    {
+        public bool Freeze = false;
+
+        private Vector2 basePosition;
+        private const float flyAmplitude = 0.5F;
+        public float Speed = 1F;
+        public float MinDistance = 5F;
+        public float AttackTimeout = 4F;
+        private float attackTimeoutTimer = 0F;
+        private float flyOffset;
+
+        private Animator beeAnimator;
+        private const string attackAnimatorParam = "Attack";
+
+        public new void Start()
+        {
+            base.Start();
+
+            basePosition = transform.position;
+            flyOffset = Random.Range(0, 1F);
+            attackTimeoutTimer = AttackTimeout;
+            beeAnimator = GetComponent<Animator>();
+        }
+
+        public void Update()
+        {
+            attackTimeoutTimer = Mathf.Max(0, attackTimeoutTimer - Time.deltaTime);
+
+            TryAttack();
+
+            if (!Freeze)
+                Fly();
+        }
+
+        private void TryAttack()
+        {
+            if (Mathf.Abs(attackTimeoutTimer) < Mathf.Epsilon)
+            {
+                beeAnimator.SetTrigger(attackAnimatorParam);
+                attackTimeoutTimer = AttackTimeout;
+            }
+        }
+
+        private void Fly()
+        {
+            if (Vector2.Distance(Player.transform.position, basePosition) > MinDistance)
+            {
+                basePosition = Vector2.MoveTowards(basePosition, Player.transform.position, Speed * Time.deltaTime);
+            }
+
+            transform.position = new Vector2(
+                basePosition.x,
+                basePosition.y + Mathf.Sin((DateTime.Now.Millisecond / 1000F + flyOffset) * 2 * Mathf.PI) * flyAmplitude);
+
+            RotateToPlayer();
+        }
+    }
+}
---
+++ Assets/Source/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Source
+{
+    /// <summary>
+    /// Enemy
+    /// </summary>
+    public class Enemy : MonoBehaviour
+    {
+        public Player Player;
+
+        private bool leftRotation = true;
+
+        public void Start()
+        {
+            Player = FindObjectOfType<Player>();
+        }
+
+        protected void RotateToPlayer()
+        {
+            if (leftRotation && Player.transform.position.x > transform.position.x)
+            {
+                leftRotation = false;
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (!leftRotation && Player.transform.position.x < transform.position.x)
+            {
+                leftRotation = true;
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+}
